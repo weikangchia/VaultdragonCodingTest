c5f2b211427f6bcd0ac650c9c2950a4ecd044b79
Wei Kang <weikangchia@gmail.com>
2016-12-14 11:11:10 +0800

    Add database test for ApiTest

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -6,27 +6,57 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiTest extends TestCase
 {
-    use DatabaseTransactions;
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        Artisan::call('db:seed', ['--class' => 'DatabaseSeeder', '--database' => 'testing']);
+    }
+
+    public function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
 
     public function testInvalidObjectApiMethod()
     {
-        $response = $this->call('GET', 'api/v1/object');
-
-        $this->assertEquals(405, $response->getStatusCode());
+        $this->json('GET', 'api/v1/object')
+             ->assertResponseStatus(405);
     }
 
-    public function testNotExistObjectKeyValue()
+    public function testNonExistObjectKeyValue()
     {
-        $response = $this->call('GET', 'api/v1/object/doesnotexits');
-
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->json('GET', 'api/v1/object/doesnotexits')
+             ->assertResponseStatus(404);
     }
 
     public function testExistObjectKeyValue()
     {
-        $response = $this->call('GET', 'api/v1/object/mykey');
+        $this->json('GET', '/api/v1/object/key1')
+             ->seeJson([
+                 'status_code' => 200
+             ])
+             ->assertResponseStatus(200);
+    }
 
-        $this->assertEquals(200, $response->getStatusCode());
+    public function testSubmitObjectValueSuccessfully()
+    {
+        $this->json('POST', '/api/v1/object', ['key10' => 'value10'])
+             ->seeJson([
+                 'status_code' => 201
+             ])
+             ->assertResponseStatus(201);
+    }
+
+    public function testSubmitEmptyObjectValue()
+    {
+        $this->json('POST', '/api/v1/object', [''])
+             ->seeJson([
+                 'status_code' => 400,
+                 'message'     => 'Your value is empty.'
+             ])
+             ->assertResponseStatus(400);
     }
 
 }
